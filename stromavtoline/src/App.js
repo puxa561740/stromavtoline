bc9a06b53fbf7563fe9634215713ab220dd36599
puxa561740 <puxa561740@gmail.com>
2024-04-29 11:16:10 +0300

    header fix

--- stromavtoline/src/App.js
+++ stromavtoline/src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <>
       <Header/>
       <main id="main">  
-        <Outlet />
+        <Outlet/>
       </main>
       <Footer/>
     </>
--- stromavtoline/src/components/Main/main.jsx
+++ stromavtoline/src/components/Main/main.jsx
@@ -1,10 +1,16 @@
 import './main.css'
 
 export default function Main() {
+    const mainTest = 20;
+    const arraiTest = [];
+    for (let i = 0; i < mainTest; i++) {
+        arraiTest.push(<div className='mainBlockTest'>Test Block {i+1}</div>)
+    }
+    console.log(arraiTest)
     return (
         <>
          <section>
-            Main 
+            {arraiTest.map(function(item){return item})}
          </section>
         </>
     )
